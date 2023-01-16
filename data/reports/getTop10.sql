3983f696c517fa5614ae95f350aaa109314eafcd
Avishag Saban <40865482+avishsa@users.noreply.github.com>
2023-01-16 14:52:07 +0200

    Update getTop10.sql

--- data/reports/getTop10.sql
+++ data/reports/getTop10.sql
@@ -1,9 +1,8 @@
-SELECT [id],
-    [ISOCODE],
-    [ReportDate],
-    [Total_cases],
-    [New_cases],
-    [Total_deaths]
-FROM [dbo].[reports]
-
-ORDER BY ReportDate;
+Select TOP 10 t1.total_cases,t1.total_deaths,t3.location,t2.rd
+From [CORONA].[dbo].[reports] t1,  (select max(reportdate) rd,ISOCODE
+  FROM [CORONA].[dbo].[reports]
+  Group by ISOCODE) t2,
+  [CORONA].[dbo].[countries] t3
+  where t1.reportdate=t2.rd AND t1.ISOCODE =t2.ISOCODE
+  AND t1.ISOCODE =t3.ISOCODE
+  ORDER BY  t1.total_cases desc  ;
